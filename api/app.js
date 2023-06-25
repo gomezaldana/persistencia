8cd075ab9da0c4533a718bc94e408b0579b4a209
enzocolocho <enzotomas.colocho@estudiantes.unahur.edu.ar>
2023-06-25 18:01:57 -0300

    agrego jwt, swagger ya terminado, esperando correccion para pasarlo a main

--- api/app.js
+++ api/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 //swagger
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/configSwagger');
+//jwt
+const jwt = require('jsonwebtoken');
 
 //cambio
 var path = require('path');
@@ -19,7 +21,53 @@ var materiaRouter = require('./routes/materia');
 var profesorRouter = require('./routes/profesor');
 var facultadRouter = require('./routes/facultad');
 
+//JSONWEBTOKEN
+app.get("/api", (req, res) => {
+  res.json({
+    mensaje: "hola mundo"
+  })
+})
 
+app.post("/api/login", (req, res) => {
+  const user = {
+    id: 1,
+    nombre: "Pablo Marcelli",
+    email: "profepablomarcelli@email.com"
+  }
+
+  jwt.sign({ user }, 'secretKey', { expiresIn: '50s' }, (err, token) => {
+    res.json({
+      token
+    })
+  });
+})
+
+app.post("/api/home", verifyToken, (req, res) => {
+
+  jwt.verify(req.token, 'secretKey', (error, authData) => {
+    if (error) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        mensaje: "Bienvenido al home",
+        authData
+      });
+    }
+  });
+})
+
+// Authorization: Bearer <token>
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+
+  if (typeof bearerHeader !== 'undefined') {
+    const bearerToken = bearerHeader.split(" ")[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,12 +86,12 @@ app.use('/fac', facultadRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
